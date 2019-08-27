efbefa007a2edc786ea6495ddeaec4fe5b42e587
Syed Muhammad Abuzar Rizvi <syedabuzar12@gmail.com>
2019-08-27 13:54:16 +0500

    18th programming practice is added.
    18th programming practice is added.
    InshaAllah next will be added soon.

---
+++ 18/18.py
@@ -0,0 +1,24 @@
+import re
+value = []
+inp = "ABd1234@1,a F1#,2w3E*,2We3345"
+#inp = input()
+items=[x for x in inp.split(',')]
+for p in items:
+    if len(p)<6 or len(p)>12:
+        continue
+    else:
+        pass
+    if not re.search("[a-z]",p):
+        continue
+    elif not re.search("[0-9]",p):
+        continue
+    elif not re.search("[A-Z]",p):
+        continue
+    elif not re.search("[$#@]",p):
+        continue
+    elif re.search("\s",p):
+        continue
+    else:
+        pass
+    value.append(p)
+print(",".join(value))
